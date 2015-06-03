01b8602ef4a8d25a5049d42fb9af1f58f7ebe43d
Diogo de Campos <campos@esss.com.br>
2015-06-03 13:42:19 -0300

    Add support for jinja2 evaluation of environment.yml
    This is simlar to what 'conda_build' does.

--- utils/travis-run-install.sh
+++ utils/travis-run-install.sh
@@ -44,7 +44,7 @@ main_install() {
         *) ;;
     esac
 
-    python -m pip install psutil ruamel.yaml pycosat pycrypto requests==2.12.4 pyopenssl==16.2.0
+    python -m pip install psutil ruamel.yaml pycosat pycrypto requests==2.12.4 pyopenssl==16.2.0 jinja2
 
     case "${TRAVIS_PYTHON_VERSION:-PYTHON_VERSION}" in
       '2.7')
